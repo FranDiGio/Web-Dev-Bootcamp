0aec76d45bcf48190133f2f525694477e6a5fcad
FranDiGio <francodvf06@gmail.com>
2024-03-31 15:56:30 +0800

    Solve React Keeper challenge

---
+++ Back-end/10.11 React Keeper App/src/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    <div className="note">
+        <h1>New Note</h1>
+        <p>I don't actually like seafood.</p>
+    </div>
+}
+
+export default Note;
